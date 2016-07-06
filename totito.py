b51fc848aaade85cde1563cd186a31aed9db10bd
jose123456879 <totitosst@gmail.com>
2016-07-06 08:43:52 -0600

    intento#1

---
+++ totito.py
@@ -0,0 +1,19 @@
+A=[]
+B=[]
+C=[]
+D=[]
+E=[]
+F=[]
+G=[]
+H=[]
+I=[]
+win=False
+TABLERO=(A+"  "+B+"  "+C+"\n"+D+"  "+E+"  "+F+"\n"+G+"  "+H+"  "+I+)
+print(TABLERO)
+while win ==False:
+	casillap1=input("ingrese una casilla(A-I): ")
+	if casillap1==a:
+		A.append("X")
+	casillap2=input("ingrese una casilla(A-I): ")
+ 
+
