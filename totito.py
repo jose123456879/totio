8ca1a0c94b090b6845bd065fe15a4bfa61527e2d
jose123456879 <totitosst@gmail.com>
2016-07-07 08:45:36 -0600

    continuacion

--- totito.py
+++ totito.py
@@ -1,19 +1,38 @@
-A=[]
-B=[]
-C=[]
-D=[]
-E=[]
-F=[]
-G=[]
-H=[]
-I=[]
+A="A"
+B="B"
+C="C"
+D="D"
+E="E"
+F="F"
+G="G"
+H="H"
+I="I"
 win=False
-TABLERO=(A+"  "+B+"  "+C+"\n"+D+"  "+E+"  "+F+"\n"+G+"  "+H+"  "+I+)
+TABLERO=(A+"_| "+B+"  "+C+"\n"+D+"  "+E+"  "+F+"\n"+G+"  "+H+"  "+I)
 print(TABLERO)
 while win ==False:
-	casillap1=input("ingrese una casilla(A-I): ")
-	if casillap1==a:
-		A.append("X")
-	casillap2=input("ingrese una casilla(A-I): ")
+	print("turno 1")
+	print(TABLERO)
+	jugador1=input("ingrese una casilla(A-I): ")
+	if jugador1==A and A!="0":
+		A="X"
+	if jugador1==B and B!="0":
+		B="X"
+	if jugador1==C and C!="0":
+		C="X"
+	if jugador1==D and D!="0":
+		D="X"
+	if jugador1==E and E!="0":
+		E="X"
+	if jugador1==F and F!="0":
+		F="X"	
+	if jugador1==G and G!="0":
+		F="X"
+	if jugador1==H and H!="0":
+		H="X"
+	if jugador1==I and I!="0":
+		I="X"
+
+		jugador2=input("ingrese una casilla(A-I): ")
  
 
