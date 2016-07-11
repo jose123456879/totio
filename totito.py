38a14f6f9887eaa1c4b80dc0249592f57f5e7b39
jose123456879 <totitosst@gmail.com>
2016-07-11 08:42:05 -0600

    quick fix
    se realizaron unos arreglos y se continuo

--- totito.py
+++ totito.py
@@ -8,7 +8,7 @@ G="G"
 H="H"
 I="I"
 win=False
-TABLERO=(A+"_| "+B+"  "+C+"\n"+D+"  "+E+"  "+F+"\n"+G+"  "+H+"  "+I)
+TABLERO=(A+"_| "+B+"_|_"+C+"\n"+D+"_| "+E+"_|_"+F+"\n"+G+" | "+H+" | "+I)
 print(TABLERO)
 while win ==False:
 	print("turno 1")
@@ -16,23 +16,42 @@ while win ==False:
 	jugador1=input("ingrese una casilla(A-I): ")
 	if jugador1==A and A!="0":
 		A="X"
-	if jugador1==B and B!="0":
+	if jugador1==B and B!="o":
 		B="X"
-	if jugador1==C and C!="0":
+	if jugador1==C and C!="o":
 		C="X"
-	if jugador1==D and D!="0":
+	if jugador1==D and D!="o":
 		D="X"
-	if jugador1==E and E!="0":
+	if jugador1==E and E!="o":
 		E="X"
-	if jugador1==F and F!="0":
+	if jugador1==F and F!="o":
 		F="X"	
-	if jugador1==G and G!="0":
+	if jugador1==G and G!="o":
 		F="X"
-	if jugador1==H and H!="0":
+	if jugador1==H and H!="o":
 		H="X"
-	if jugador1==I and I!="0":
+	if jugador1==I and I!="o":
 		I="X"
 
-		jugador2=input("ingrese una casilla(A-I): ")
- 
+	print(TABLERO)
+	jugador2=input("ingrese una casilla(A-I): ")
 
+	if jugador2==A and A!="X"
+		A="o"		
+	if jugador2==B and B!="X"
+		B="o"
+	if jugador2==C and C!="X"
+		C="o"	
+	if jugador2==D and D!="X"
+		D="o"						
+	if jugador2==E and E!="X"
+		E="o"			
+	if jugador2==F and F!="X"
+		F="o"		
+	if jugador2==G and G!="X"
+		G="o"	
+	if jugador2==H and H!="X"
+		H="o"
+	if jugador2==I and I!="X"
+		I="o"			
+	print(TABLERO)	
